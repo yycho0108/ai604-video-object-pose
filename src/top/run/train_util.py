c43a6ed0d1c40f3c5e36863d32e5fd0eb89da684
JiyongAhn_Desktop <ajy8456@gmail.com>
2021-04-20 16:27:16 +0900

    make Logger

--- src/top/run/train_util.py
+++ src/top/run/train_util.py
@@ -7,6 +7,10 @@ from simple_parsing import Serializable
 import torch as th
 import logging
 
+import os
+import time
+from torch.utils.tensorboard import SummaryWriter
+
 
 def _resolve_device(device: Union[None, str, th.device]):
     """ Resolve torch device. """
@@ -231,10 +235,45 @@ class Trainer(object):
             Saver(self.model, self.optim).save('/tmp/model-backup.zip')
 
 
+
+class Logger(object):
+    def __init__(self, save_dir):
+        """Create a summary writer logging to log_dir."""
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        time_str = time.strftime('%Y-%m-%d-%H-%M')
+        log_dir = save_dir + '/logs_{}'.format(time_str)
+        self.writer = SummaryWriter(log_dir=log_dir)
+        self.log = open(log_dir + '/log.txt', 'w')
+        self.start_line = True
+
+    def write(self, txt):
+        if self.start_line:
+            time_str = time.strftime('%Y-%m-%d-%H-%M')
+            self.log.write('{}: {}'.format(time_str, txt))
+        else:
+            self.log.write(txt)  
+            self.start_line = False
+        if '\n' in txt:
+            self.start_line = True
+            self.log.flush()
+    
+    def close(self):
+        self.log.close()
+    
+    def scalar_summary(self, tag, value, step):
+        """Log a scalar variable."""
+        self.writer.add_scalar(tag, value, step)
+
+
 def main():
     # NOTE(ycho): Currently missing `loss_fn`
     #trainer = Trainer(Trainer.Settings(),
     #                  model, optimizer, callbacks, train_loader)
+
+    logger = Logger('./')
+    logger.write('test')
+    logger.close()
 
 
 if __name__ == '__main__':
