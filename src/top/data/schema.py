b079fad10e245f15d95725b838f942e3f0e6e80c
Yoonyoung Cho <jchocholate@gmail.com>
2021-04-27 20:23:03 +0900

    add schema

---
+++ src/top/data/schema.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+
+class
+
+
+class Schema(Enum):
+    """
+    Set of fixed constants to refer to contents from our dataset interfaces.
+    NOTE(ycho): Generally intended to follow the Objectron schema.
+    """
+    IMAGE = "image"
+    KEYPOINT_2D = "point_2d"
+    INSTANCE_NUM = "instance_num"
+    TRANSLATION = "object/translation"
+    ORIENTATION = "object/orientation"
+    SCALE = "object/scale"
+    CLASS = "object/class"  # NOTE(ycho): Class index, integer.
+    PROJECTION = "camera/projection"
+
+
+def main():
+    a = {'image': 5}
+
+
+if __name__ == '__main__':
+    main()
